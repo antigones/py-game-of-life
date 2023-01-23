b122d9da733fb162fdee5c0d91c1629e48cc9b76
antigones <juna.salviati@gmail.com>
2023-01-23 22:34:05 +0100

    size inference refactor

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # py-game-of-life
-Simple Game of Life implementation in Python
+Simple (console-animated) Game of Life implementation in Python.
+
+Usage:
+
+    def draw_glider(on_grid):
+        on_grid[3, 1] = 1
+        on_grid[3, 2] = 1
+        on_grid[3, 3] = 1
+        on_grid[2, 3] = 1
+        on_grid[1, 2] = 1
+    
+    size= 11
+    n_gen = 50
+    grid = np.zeros(shape=(size,size))
+
+    draw_glider(grid)
+
+    gol = GameOfLife(grid, n_gen)
+    steps = gol.generate_steps()
+
+generate_steps() returns a list of animation steps to be used to compose an animated gif or with urwid:
+
+![Game of Life output](./game_of_life.gif)
--- gol.py
+++ gol.py
@@ -4,13 +4,14 @@ import numpy as np
 
 class GameOfLife:
 
-    def __init__(self, grid, size, n_gen):
+    def __init__(self, grid, n_gen):
         self.grid = grid
-        self.size = size
         self.n_gen = n_gen
+        self.size_y = len(grid)
+        self.size_x = len(grid[0])
     
 
-    def count_alive_neighbours(self, on_grid, size, i, j):
+    def count_alive_neighbours(self, on_grid, i, j):
         neighbors = [
             (i-1, j),   # n
             (i-1, j-1), # nw
@@ -23,13 +24,13 @@ class GameOfLife:
             ]
 
         return sum(on_grid[x][y] == 1 for x, y in neighbors
-                if 0 <= x < size and 0 <= y < size)
+                if 0 <= x < self.size_x and 0 <= y < self.size_y)
 
 
-    def next_generation(self, on_grid, size):
-        next_grid = np.zeros(shape=(size, size))
-        for (i, j) in [(i, j) for i in range(size) for j in range(size)]:
-            alive_n = self.count_alive_neighbours(on_grid, size, i, j)
+    def next_generation(self, on_grid):
+        next_grid = np.zeros(shape=(self.size_y, self.size_x))
+        for (i, j) in [(i, j) for i in range(self.size_y) for j in range(self.size_x)]:
+            alive_n = self.count_alive_neighbours(on_grid, i, j)
             
             cur_cell = on_grid[i, j]
             if cur_cell == 1:
@@ -52,11 +53,11 @@ class GameOfLife:
     def generate_steps(self):
         steps = []
         steps.append(self.grid)
-        o = self.next_generation(self.grid, self.size)
+        o = self.next_generation(self.grid)
         steps.append(o)
 
         for _ in range(self.n_gen-1):
-            o = self.next_generation(o, self.size)
+            o = self.next_generation(o)
             steps.append(o)
         
         return steps
